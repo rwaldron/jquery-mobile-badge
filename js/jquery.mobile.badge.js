1539dd3b69f0ac08cb2283519fe942b733ad2100
Rick Waldron <waldron.rick@gmail.com>
2012-01-18 16:29:31 -0500

    Initial qunit lib

--- js/jquery.mobile.badge.js
+++ js/jquery.mobile.badge.js
@@ -1,18 +1,40 @@
 (function( window, document, $, undefined ) {
 
-var $doc = document;
-
 $.widget( "mobile.badge", $.mobile.widget, {
 	selector: ":jqmData(badge)",
 	_create: function() {
-		var self = $( this.element );
+		var self = $( this.element ),
+				owner = self.prev(),
+				value = self.html(),
+				template = [
+					"<div class='ui-badge-outer'>",
+					"<div class='ui-badge-inner'>",
+					"<span class='ui-badge'>",
+					value,
+					"</span></div></div>"
+				].join("");
 
-		console.log( self );
+
+		console.log(
+			owner,
+			owner.offset(),
+			owner.position().top,
+			owner.position().left,
+			owner.css("top")
+		);
+
+		// Remove Place Holder
+		self.remove();
+
+		// Insert rendered badge inside owner
+		owner.append(
+			$(template)
+		);
 	}
 });
 
 // Initialize declarative badges in markup
-$doc.bind( "pagecreate create", function( event ) {
+$( document ).bind( "pagecreate create", function( event ) {
 	$( $.mobile.badge.prototype.selector, event.target ).badge();
 });
 
